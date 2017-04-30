9f67a4444943f6dab160cc39ffd4953383766d51
Ludwig Feed <ludwig.feed@hotmail.com>
2017-04-30 20:47:42 +0200

    fix terminateList, added moveReview

--- functions/index.js
+++ functions/index.js
@@ -37,48 +37,17 @@ function GetRealExpiryDate(expdate) {
 
 
 // Publish a list. Moves it from unpublished_lists to published_lists and publishes geopoints
-// Parameters: list_key, token
-// - list_key: chiave della lista unpublished da pubblicare
-// - token
-//exports.publishList = functions.https.onRequest((req, res) => {
 exports.publishList = functions.database.ref('/published_lists/{userId}/{listId}').onWrite(event => {
-
-
+  
   // Only edit data when it is first created.
   if (event.data.exists() && !event.data.previous.exists()) {
 
-    //let idToken = req.query.token;
-    //console.log("validating token: " + idToken);
-
-    // result object. Must contain two properties:
-    // - error: boolean indicating is the request has failed
-    // - errorMessage: string, empty if boolean is true
-    //let result = {};
-    //result.error = true;
-    //result.errorMessage = "";
-
-    //admin.auth().verifyIdToken(idToken)
-    //.then(function (decodedToken) {
-    //let uid = decodedToken.uid;
-    //console.log("Token validated: " + uid);
-    //let list_key = req.query.list_key;
-    //if (list_key == null || list_key == "") {
-    //  console.warn("Null list_key");
-    //  result.error = true;
-    //  result.errorMessage = "Null list_key";
-    //  res.send(200, JSON.stringify(result));
-    //} else {
-    //return once(admin.database().ref('/unpublished_lists/' + uid + '/' + list_key)).then(unpub_list => {
-    //return event.data.ref.parent.once("value", (_unpub_list) => {
-    let pub_list = event.data.val();
-    //if (unpub_list != null) {
     let uid = event.params.userId;
     let list_key = event.params.listId;
+    let pub_list = event.data.val();
+
     delete pub_list.Publish;
     console.log("Publishing list and geopoints...");
-    // pubblico su published_lists
-    //return admin.database().ref('/published_lists/' + uid).push(unpub_list).then(pub_list => {
-    //console.log("List Published! Publishing geopoints...");
     let promises = [];
     for (let address_key in pub_list.DeliveryAddresses) {
       let geo = {};
@@ -98,9 +67,6 @@ exports.publishList = functions.database.ref('/published_lists/{userId}/{listId}
         }).catch((err) => {
           console.warn("Cannot publish geopoint: " + err.message);
           return;
-          //result.error = true;
-          //result.errorMessage = "Cannot publish geopoint: " + err.message;
-          //res.send(200, JSON.stringify(result));
         })
       );
     }
@@ -114,32 +80,6 @@ exports.publishList = functions.database.ref('/published_lists/{userId}/{listId}
       return;
     });
 
-
-    //}).catch(err => {
-    //  console.warn("Cannot push to published_lists: " + err.message);
-    //  return;
-    //  //result.error = true;
-    //  //result.errorMessage = "cannot push to published_lists: " + err.message;
-    //  //res.send(200, JSON.stringify(result));
-    //});
-    //} else {
-    //  return;
-    //}
-    //} else {
-    //  console.warn("list_key not existing");
-    //  result.error = true;
-    //  result.errorMessage = "list_key not existing";
-    //  res.send(200, JSON.stringify(result));
-    //}
-    //    });
-    //  }
-    //}).catch(function (error) {
-    //  console.warn("Token NOT validated: " + error);
-    //  result.error = true;
-    //  result.errorMessage = "Token NOT validated: " + error;
-    //  res.send(200, JSON.stringify(result));
-    //});
-    //});
   } else {
     return;
   }
@@ -183,7 +123,6 @@ exports.addCandidate = functions.database.ref('/candidatures/{userId}/{candidatu
       new_candidate.Comment = Comment || "";
 
       return once(admin.database().ref('/users/' + event.params.userId + '/DisplayName')).then(user_name => {
-        //let user_name = _name.val();
         new_candidate.DisplayName = user_name;
         console.log('DisplayName: ' + user_name);
         return admin.database().ref('/candidates/' + ListOwnerUid).push(new_candidate).then(() => {
@@ -206,44 +145,36 @@ exports.acceptCandidate = functions.database.ref('/published_lists/{userId}/{lis
 
     try {
       // Only edit data when it is first created or changes from empty to some value.
-      if (event.data.exists() && event.data.child('ChosenCandidateKey').exists() && (!event.data.child('ChosenCandidateKey').previous.exists() || event.data.child('ChosenCandidateKey').previous.val() == "")) {
+      if (event.data.exists() && event.data.child('ChosenCandidateKey').exists() && !event.data.child('ChosenCandidateKey').previous.exists()) {
 
+        let uid = event.params.userId;
+        let list_key = event.params.listId;
+        let pub_list = event.data.val();
 
         // Grab the current value of what was written to the Realtime Database.
         let acceptedCandidateKey = event.data.child('ChosenCandidateKey').val();
-        console.log('Candidate <' + acceptedCandidateKey + '> accepted for list <' + event.params.listId + '> of user <' + event.params.userId + '>');
+        console.log('Candidate <' + acceptedCandidateKey + '> accepted for list <' + list_key + '> of user <' + uid + '>');
 
-        //console.log('/candidates/' + event.params.userId + '/' + acceptedCandidateKey);
-        //promises.push(
-
-        return once(admin.database().ref('/candidates/' + event.params.userId + '/' + acceptedCandidateKey)).then(candidate => {
-
-          //return event.data.ref.parent.once("value", (_pub_list) => {
-          let pub_list = event.data.val();
+        return once(admin.database().ref('/candidates/' + uid + '/' + acceptedCandidateKey)).then(candidate => {
 
           //all the promises the function will create
           let promises = [];
-
-          //let candidate = _candidate.val();
+          
           console.log("candidate: " + JSON.stringify(candidate));
 
           promises.push(
             event.data.ref.update({ ChosenCandidatureKey: candidate.CandidatureReferenceKey, ChosenShopperUid: candidate.uid, ChosenShopperName: candidate.DisplayName }).then(() => {
               console.log("Shopper data Updated");
-              //Promise.resolve();
             }).catch((err) => {
               console.warn("Cannot update shopper data: " + err.message);
-              //Promise.resolve();
             })
           );
 
           promises.push(
             event.data.ref.child('DeliveryAddresses').child(candidate.AddressKey).child('Chosen').set(true).then(() => {
               console.log("Updated chosen address.");
-              //Promise.resolve();
             }).catch((err) => {
               console.warn("Cannot update chosen address: " + err.message);
-              //Promise.resolve();
             })
           );
 
@@ -252,7 +183,6 @@ exports.acceptCandidate = functions.database.ref('/published_lists/{userId}/{lis
               console.log("Shopper set as Accepted.");
             }).catch((err) => {
               console.warn("Cannot set accepted to shopper's candidature: " + err.message);
-              //Promise.resolve();
             })
           );
 
@@ -262,14 +192,9 @@ exports.acceptCandidate = functions.database.ref('/published_lists/{userId}/{lis
           }
           promises.push(
             admin.database().ref("geopoints").update(geopoint_to_update).then(function () {
-              //counter++;
-              //if (counter >= num_addresses) {
               console.log("All geopoints removed. ");
-              //Promise.resolve();
-              //}
             }).catch((err) => {
               console.warn("Cannot remove geopoint: " + err.message);
-              //Promise.resolve();
             })
           );
 
@@ -278,17 +203,12 @@ exports.acceptCandidate = functions.database.ref('/published_lists/{userId}/{lis
             return;
           });
 
-          //}).catch((err) => {
-          //  console.warn("Cannot retrieve list: " + err.message);
-          //  return;
-          //})
-
 
         }).catch((err) => {
           console.warn("Cannot retrieve candidate: " + err.message);
           return;
         });
-        //);
+
       } else {
         return;
       }
@@ -303,71 +223,76 @@ exports.acceptCandidate = functions.database.ref('/published_lists/{userId}/{lis
 
 // Terminates list when payment is done.
 // Moves list from published_lists to terminated_lists (both for shopper and demander). 
-exports.terminateList = functions.database.ref('/published_lists/{userId}/{listId}/TerminatedDate')
+exports.terminateList = functions.database.ref('/published_lists/{userId}/{listId}')
   .onWrite(event => {
 
     try {
 
       let uid = event.params.userId;
       let list_key = event.params.listId;
+      let pub_list = event.data.val();
 
-      // get list value
-      return event.data.ref.parent.once("value", (_pub_list) => {
-        let pub_list = _pub_list.val();
-        if (pub_list == null)
-          return;
-        pub_list.ReviewLeft = false;
-        for (let addressKey in pub_list.DeliveryAddresses) {
-          if (pub_list.DeliveryAddresses[addressKey].Chosen != true) {
-            delete pub_list.DeliveryAddresses[addressKey];
+      if (event.data.exists() && event.data.child('TerminatedDate').exists() && !event.data.child('TerminatedDate').previous.exists()) {
+
+        return once(admin.database().ref('/candidates/' + uid)).then(candidates => {
+
+          let promises = [];
+
+          for (let candidateKey in candidates) {
+            let candidate = candidates[candidateKey];
+            if (candidate.ListReferenceKey == list_key) {
+              promises.push(
+                admin.database().ref('/candidates/' + uid + '/' + candidateKey).remove()
+              );
+              promises.push(
+                admin.database().ref('/candidatures/' + candidate.uid + '/' + candidate.CandidatureReferenceKey).remove()
+              );
+            }
           }
-        }
-        if (pub_list != null) {
-          console.log("Copying list in terminated_lists for demander...");
-          return admin.database().ref('/terminated_lists/' + uid + '/as_demander/' + list_key).set(pub_list).then(() => {
-            console.log("Copying list in terminated_lists for shopper...");
-            return admin.database().ref('/terminated_lists/' + pub_list.ChosenShopperUid + '/as_shopper/' + list_key).set(pub_list).then(() => {
-              console.log("List copied! removing from published_lists...");
-              return admin.database().ref('/published_lists/' + uid + '/' + list_key).remove().then(() => {
-                console.log("List removed from published_lists. Removing candidatures and candidates");
-                return admin.database().ref('/candidates/' + uid + '/' + list_key).once("value", (_candidates) => {
-                  let candidates = _candidates.val();
-                  let counter = 0;
-                  let num_candidates = Object.keys(candidates).length;
-                  for (let candidateKey in candidates) {
-                    let candidate = candidates[candidateKey];
-                    admin.database().ref('/candidatures/' + candidate.uid + '/' + candidate.CandidatureReferenceKey).remove().then(() => {
-                      counter++;
-                      if (counter >= num_candidates) {
-                        console.log("Candidatures removed. Removing candidates...");
-                        return admin.database().ref('/candidates/' + uid + '/' + list_key).remove().then(() => {
-                          console.log("All done");
-                          return;
-                        });
-                      }
-                    });
-                  }
-                }).catch(err => {
-                  console.warn("Cannot remove published_lists: " + err.message);
-                  return;
-                });
-              }).catch(err => {
-                console.warn("Cannot remove published_lists: " + err.message);
-                return;
-              });
+
+          pub_list.ReviewLeft = false;
+          for (let addressKey in pub_list.DeliveryAddresses) {
+            if (pub_list.DeliveryAddresses[addressKey].Chosen != true) {
+              delete pub_list.DeliveryAddresses[addressKey];
+            }
+          }
+
+          promises.push(
+            admin.database().ref('/terminated_lists/' + uid + '/as_demander/' + list_key).set(pub_list).then(() => {
+              console.log("Copied list in terminated_lists for demander.");
+            }).catch(err => {
+              console.warn("Cannot copy to terminated_lists for demander: " + err.message);
+              return;
+            })
+          );
+
+          promises.push(
+            admin.database().ref('/terminated_lists/' + pub_list.ChosenShopperUid + '/as_shopper/' + list_key).set(pub_list).then(() => {
+              console.log("Copied list in terminated_lists for shopper.");
             }).catch(err => {
               console.warn("Cannot push to terminated_lists for shopper: " + err.message);
               return;
-            });
-          }).catch(err => {
-            console.warn("Cannot push to terminated_lists for demander: " + err.message);
-            return;
+            })
+          );
+
+          promises.push(
+            admin.database().ref('/published_lists/' + uid + '/' + list_key).remove().then(() => {
+              console.log("List removed from published_lists.");
+            }).catch(err => {
+              console.warn("Cannot remove published_lists: " + err.message);
+              return;
+            })
+          );
+
+          return Promise.all(promises).then(() => {
+            console.log("All done.");
           });
-        } else {
-          console.warn("Null pub_list");
-          return;
-        }
-      });
+
+        });
+
+      } else {
+        return;
+      }
 
     } catch (e) {
       console.warn('Caught error: ' + e);
@@ -375,3 +300,70 @@ exports.terminateList = functions.database.ref('/published_lists/{userId}/{listI
     }
 
   });
+
+
+
+// Move review from reviewer to reviewee
+exports.moveReview = functions.database.ref('/reviews/{userId}/to_move/{reviewId}')
+  .onWrite(event => {
+
+    if (event.data.exists() && !event.data.previous.exists()) {
+
+      let review = event.data.val();
+
+      let promises = [];
+
+      promises.push(
+        admin.database().ref('/reviews/' + review.RevieweeUid + '/done').push(review).then(() => {
+          console.log("Review moved");
+        })
+      );
+
+      promises.push(
+        event.data.ref.remove().then(() => {
+          console.log("Review removed from reviewer");
+        })
+      );
+
+      return Promise.all(promises).then(() => {
+        console.log("All done");
+      });
+
+    } else {
+      return;
+    }
+
+  });
+
+
+
+// OLD CODE
+
+// Parameters: list_key, token
+// - list_key: chiave della lista unpublished da pubblicare
+// - token
+//exports.publishList = functions.https.onRequest((req, res) => {
+
+    //let idToken = req.query.token;
+    //console.log("validating token: " + idToken);
+
+    // result object. Must contain two properties:
+    // - error: boolean indicating is the request has failed
+    // - errorMessage: string, empty if boolean is true
+    //let result = {};
+    //result.error = true;
+    //result.errorMessage = "";
+
+    //admin.auth().verifyIdToken(idToken)
+    //.then(function (decodedToken) {
+    //let uid = decodedToken.uid;
+    //console.log("Token validated: " + uid);
+    //let list_key = req.query.list_key;
+    //if (list_key == null || list_key == "") {
+    //  console.warn("Null list_key");
+    //  result.error = true;
+    //  result.errorMessage = "Null list_key";
+    //  res.send(200, JSON.stringify(result));
+    //} else {
+    //return once(admin.database().ref('/unpublished_lists/' + uid + '/' + list_key)).then(unpub_list => {
+    //return event.data.ref.parent.once("value", (_unpub_list) => {
